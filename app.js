f1a78343c1ceb371e370f81e574c81334ebadb85
Abdulbaasit <baasisek01@gmail.com>
2020-04-29 18:15:14 +0100

    Fixed the add event listener Irregularity

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
 const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
 
+const startProgram = debounce(() => {
+	console.log(`Starting Up The Program`);
+}, 1000);
 // Watch the current directory
 chokidar.watch('.')
 	// When a file is added or chokidar sees(registers the file existence) for the first time
-	.on("add", () => console.log(`File Added`))
+	.on("add", startProgram)
 	.on("change", () => console.log(`File Changed`))
 	// When a file is deleted
 	.on("unlink", () => console.log(`File Deleted`));
