61eb7f076434218c70a3c23f52ac8e89bb07a1c9
Abdulbaasit <baasisek01@gmail.com>
2020-04-29 17:57:33 +0100

    File watching Functionality

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log(`I was executed`);
+const chokidar = require('chokidar');
+
+// Watch the current directory
+chokidar.watch('.')
+	// When a file is added or chokidar sees(registers the file existence) for the first time
+	.on("add", () => console.log(`File Added`))
+	.on("change", () => console.log(`File Changed`))
+	// When a file is deleted
+	.on("unlink", () => console.log(`File Deleted`));
