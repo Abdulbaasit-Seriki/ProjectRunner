a699a85c1053da29ffc94766ea1bb429a46bb8bc
Abdulbaasit <baasisek01@gmail.com>
2020-04-30 11:04:14 +0100

    Setup child processes

--- app.js
+++ app.js
@@ -2,14 +2,39 @@
 
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
+const program = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
 
-const startProgram = debounce(() => {
-	console.log(`Starting Up The Program`);
-}, 1000);
-// Watch the current directory
-chokidar.watch('.')
-	// When a file is added or chokidar sees(registers the file existence) for the first time
-	.on("add", startProgram)
-	.on("change", () => console.log(`File Changed`))
-	// When a file is deleted
-	.on("unlink", () => console.log(`File Deleted`));
+// Gives the user an insight as how to use the CLI Tool
+program
+	.version('1.0.0')
+	.argument('[filename]', `Name Of The File To Execute`)
+	.action( async ({ filename }) => {
+
+		const name = filename || 'app.js';
+		try {  
+			// Checks if the file exists on the users macjine
+			await fs.promises.access(name);
+		}
+		catch(err) {
+			throw new Error(`Can't find file ${name}`);
+		}
+
+		const startProgram = debounce(() => {
+			// When startProgram is called, execute the file. The third argument(object) tells it to log all the
+			// to the parrent program
+			spawn('node', [name], { stdio: 'inherit' });
+		}, 1000);
+
+		// Watch the current directory
+		chokidar.watch('.')
+			// When a file is added or chokidar sees(registers the file existence) for the first time
+			.on("add", startProgram)
+			.on("change", startProgram)
+			// When a file is deleted
+			.on("unlink", startProgram);
+	});
+program.parse(process.argv);
+
+// Dont forget to use colors and input from the usuer
